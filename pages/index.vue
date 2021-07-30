08fea2b57ef1037633ddaa719ec1cdc3ddd93689
shaharafat <shaharafhat@gmail.com>
2021-07-31 02:42:26 +0600

    initial setup with nuxt-property-decorator

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'nuxt-property-decorator'
+
+@Component
+export default class Index extends Vue {}
+// export default Vue.extend({})
+</script>
